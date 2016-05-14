2c5990372d06b2d936b1d7a150901f5a5e6f4be0
kirin111697 <coco41134@hotmail.com>
2016-05-15 01:16:26 +0800

    fix crash but still have bug

--- gamepage.cpp
+++ gamepage.cpp
@@ -34,43 +34,83 @@ gamePage::~gamePage()
 
 void gamePage::bitmap(){
     connect(mapTMR,SIGNAL(timeout()),this,SLOT(addBit()));
-    it=keep.begin();
+    //it=keep.begin();
 }
 
 void gamePage::addBit(){
     int color=rand()%2;
-    bit=new item(color);
-    sc->addItem(bit);
-    moveTMR->start(20);
-    connect(moveTMR,SIGNAL(timeout()),bit,SLOT(moving()));
-    keep.push_back(bit);
+    if (keep.empty()){
+        qDebug() << "add first bit";
+        bit=new item(color);
+        sc->addItem(bit);
+        connect(moveTMR,SIGNAL(timeout()),bit,SLOT(moving()));
+        moveTMR->start(20);
+        keep.push_back(bit);
+        it=keep.begin();
+        qDebug() << it;
+    }
+    else{
+        bit=new item(color);
+        sc->addItem(bit);
+        connect(moveTMR,SIGNAL(timeout()),bit,SLOT(moving()));
+        moveTMR->start(20);
+        keep.push_back(bit);
+        qDebug() << "add bit";
+
+    }
 }
 
 void gamePage::keyPressEvent(QKeyEvent *hit)
 {
+    qDebug() << "key pressed";
+    //qDebug() << (*it)->x();
     if (it==keep.end())
         return;
-    if ((*it)->x() > 70){
+    if ((*it)->x() > 70 && (*it)->x() < 200){
         if (hit->key() == Qt::Key_D){
+            /*qDebug() << "still working";
             qDebug() << hit->key();
+            qDebug() << "still working";*/
             if ((*it)->getColor() == 0){
                 score++;
+                qDebug() << "add score";
                 delete *it;
                 ++it;
+                qDebug() << it;
+            }
+            else{
+                qDebug() << "wrong key";
+                delete *it;
+                ++it;
+                qDebug() << it;
             }
         }
         else if(hit->key() == Qt::Key_K){
+            //qDebug() << "still working";
             qDebug() << hit->key();
+            //qDebug() << "still working";
             if ((*it)->getColor() == 1){
+                //qDebug() << "still working";
                 score++;
                 delete *it;
                 ++it;
+                qDebug() << it;
+            }
+            else{
+                delete *it;
+                ++it;
+                qDebug() << it;
             }
         }
     }
-    else{
+    else if ((*it)->x() < 70){
+        qDebug() << "out of limit";
         delete *it;
         ++it;
+        qDebug() << it;
+    }
+    else{
+        qDebug() << "too early";
     }
 }
 
